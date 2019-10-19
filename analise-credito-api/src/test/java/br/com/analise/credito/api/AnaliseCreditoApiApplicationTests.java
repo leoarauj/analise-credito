24cfa7365b6caf3dbf38bc71cdb939aec4cb6bda
= <=>
2019-10-18 23:00:59 -0300

    Criação do projeto analise-credito-api

---
+++ analise-credito-api/src/test/java/br/com/analise/credito/api/AnaliseCreditoApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.analise.credito.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnaliseCreditoApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
