01e583daf0bcd04835cf2da094269407c4a94717
= <=>
2019-10-18 21:27:56 -0300

    Criação das classes de configuração do banco(MySQL) e atualização do application.properties com as configuraçãos de perfil de ambiente, porta do servidor, nome e versão da API - Cadastro.

---
+++ cadastro-api/src/main/java/br/com/cadastro/api/config/DataBaseConfigurationMySQL.java
@@ -0,0 +1,42 @@
+package br.com.cadastro.api.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+/**
+ * Classe de configuração do banco de dados MySQL
+ * @author Leonardo Araújo
+ */
+@Configuration
+@Profile("dev")
+public class DataBaseConfigurationMySQL {
+
+	@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/cadastro?serverTimezone=UTC");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/analise_credito?createDatabaseIfNotExist=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+    }
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
+}
