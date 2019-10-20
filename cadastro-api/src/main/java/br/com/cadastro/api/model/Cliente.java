bf5927a6aea4626df8248587a1f0a705e018a462
= <=>
2019-10-20 19:05:39 -0300

    Remoção dos validadores de javax.validation

--- cadastro-api/src/main/java/br/com/cadastro/api/model/Cliente.java
+++ cadastro-api/src/main/java/br/com/cadastro/api/model/Cliente.java
@@ -9,9 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -42,38 +39,29 @@ public @Data class Cliente implements Serializable {
 	@Column(name = "PK_CLIENTE")
 	private Long id;
 
-	@NotBlank
 	@Column(name = "NOME", nullable = false, length = 100)
 	private String nome;
 
-	@NotBlank
 	@Column(name = "CPF", nullable = false, unique = true, length = 14)
 	private String cpf;
 
-	@NotNull
 	@Column(name = "IDADE", nullable = false)
 	private Integer idade;
 
-	@NotNull
 	@Convert(converter = SexoConverter.class)
 	@Column(name = "SEXO", nullable = false, length = 15)
 	private Sexo sexo;
 
-	@NotNull
 	@Convert(converter = EstadoCivilConverter.class)
 	@Column(name = "ESTADO_CIVIL", nullable = false)
 	private EstadoCivil estadoCivil;
 
-	@NotBlank
 	@Column(name = "UF", nullable = false)
 	private String uf;
 
-	@NotNull
 	@Column(name = "DEPENDENTES", nullable = false)
 	private Integer dependentes;
 
-	@NotNull
-	@Min(value = 0)
 	@Column(name = "RENDA", scale = 2, precision = 2, nullable = false)
 	private Double renda;
 
