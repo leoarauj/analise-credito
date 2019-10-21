3177650628f4b48af6052fdf2a9053192abd4d7f
= <=>
2019-10-20 23:27:27 -0300

    Implementação das funções de busca de análise de crédito por idCliente

--- analise-credito-api/src/main/java/br/com/analise/credito/api/repository/AnaliseCreditoRepository.java
+++ analise-credito-api/src/main/java/br/com/analise/credito/api/repository/AnaliseCreditoRepository.java
@@ -3,6 +3,8 @@ package br.com.analise.credito.api.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.analise.credito.api.enuns.StatusAprovadoNegado;
@@ -21,7 +23,7 @@ public interface AnaliseCreditoRepository extends JpaRepository<AnaliseCredito, 
 	 * @param status
 	 * @return
 	 */
-	List<AnaliseCredito> findByStatus(final StatusAprovadoNegado status);
+	public List<AnaliseCredito> findByStatus(final StatusAprovadoNegado status);
 
 	/**
 	 * Retorna a lista de {@link AnaliseCredito} associada ao cliente
@@ -29,5 +31,27 @@ public interface AnaliseCreditoRepository extends JpaRepository<AnaliseCredito, 
 	 * @param idCliente
 	 * @return
 	 */
-	List<AnaliseCredito> findByIdCliente(final Long idCliente);
+	public List<AnaliseCredito> findByIdCliente(final Long idCliente);
+
+	/**
+	 * Busca {@link AnaliseCredito} por idCliente e com data de análise mais recente
+	 * 
+	 * @param idCliente
+	 * @return
+	 */
+	@Query(" SELECT analise FROM AnaliseCredito analise "
+			+ " WHERE analise.idCliente = :idCliente "
+			+ " AND analise.dataAnalise = ( SELECT MAX(dataAnalise) FROM AnaliseCredito analise_2 WHERE analise_2.idCliente = :idCliente) ")
+	public AnaliseCredito findByIdClienteMaiorData(@Param("idCliente") final Long idCliente);
+
+	/**
+	 * Busca a lista de {@link AnaliseCredito} segundo lista de idsClientes e com data de análise mais recente
+	 * 
+	 * @param idsCliente
+	 * @return
+	 */
+	@Query(" SELECT analise FROM AnaliseCredito analise "
+			+ " WHERE analise.idCliente IN (:idsCliente) "
+			+ " AND analise.dataAnalise = ( SELECT MAX(dataAnalise) FROM AnaliseCredito analise_2 WHERE analise_2.idCliente = analise.idCliente) ")
+	public List<AnaliseCredito> findAnaliseCreditoByIdClienteAndMaxDateDataAnalise(@Param("idsCliente") final List<Long> idsCliente);
 }
